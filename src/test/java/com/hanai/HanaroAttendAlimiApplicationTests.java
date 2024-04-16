9aba284f9b3a0899a8b64ac813214f874cd9bafa
bigstar9906 <bigstar9906@naver.com>
2024-04-16 15:37:43 +0900

    Init

---
+++ src/test/java/com/hanai/HanaroAttendAlimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaroAttendAlimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
